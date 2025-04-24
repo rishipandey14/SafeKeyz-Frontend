196a6e4e622d260ede4c23f9c8ca3606833dc567
Rishi <hopesalive1403@gmail.com>
2025-04-24 17:21:39 +0530

    rendering LandingPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Body from './Components/Body';
 import Feed from './Components/Feed';
 import Login from './Components/Login';
 import Profile from './Components/Profile';
+import HomePage from './Components/HomePage';
+import Signup from './Components/Signup';
 
 const App = () => {
   return (
@@ -11,8 +13,10 @@ const App = () => {
       <BrowserRouter basename='/'>
         <Routes>
           <Route path='/' element={<Body />}>
+            <Route index element={<HomePage />} />
             <Route path='/feed' element={<Feed />}></Route>
             <Route path='/profile' element={<Profile />}></Route>
+            <Route path='/signup' element={<Signup />}></Route>
             <Route path='/login' element={<Login />}></Route>
           </Route>
         </Routes>

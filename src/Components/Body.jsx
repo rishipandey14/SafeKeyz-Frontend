9cc4fcad17bba3f2b72d8c4fdf95523b9ab51ffb
Rishi <hopesalive1403@gmail.com>
2025-04-27 13:49:16 +0530

    debugging

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,33 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-import { Outlet, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
-import { BASE_URL } from "../utils/constants";
-import { addUser } from "../utils/userSlice";
+import { Outlet } from "react-router-dom";
 
 const Body = () => {
-
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const userData = useSelector((store) => store.user);
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      if(userData && userData.emailId) return ;
-
-      try {
-        const res = axios.get(BASE_URL + "/profile/view", {withCredentials: true});
-        dispatch(addUser(res.data.data));
-      } catch (err) {
-        if(err.response && err.response.status === 401) return navigate("/login");
-        console.log(err);
-      }
-    };
-    fetchUser();
-  }, [userData, dispatch, navigate]);
-
 
   return (
     <div>

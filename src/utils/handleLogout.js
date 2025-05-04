0b76a1f2ab3315c72d93f1c40c63196c5af3d6cb
Rishi <hopesalive1403@gmail.com>
2025-05-04 20:03:47 +0530

    fixed logout toast issue

--- src/utils/handleLogout.js
+++ src/utils/handleLogout.js
@@ -2,11 +2,13 @@
 import axios from "axios";
 import {BASE_URL} from "./constants";
 import {removeUser} from "./userSlice";
+import { showToast } from "./toastSlice";
 
 export const handleLogout = async (dispatch, navigate) => {
   try {
     const res = await axios.post(BASE_URL + "/logout", {}, {withCredentials : true});
     dispatch(removeUser());
+    dispatch(showToast("Logged out successfully"))
     navigate("/");
   } catch (err) {
     console.error(err);

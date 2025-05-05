68c332dd0ec036b849b2ebb0204d6b3f7ff082e9
Rishi <hopesalive1403@gmail.com>
2025-05-05 13:52:06 +0530

    modified Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,15 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 fixed bottom-0">
-        <aside>
-          <p>
-            Copyright © {new Date().getFullYear()} - All right reserved by SafeKeyz
-          </p>
-        </aside>
-      </footer>
-    </div>
+    <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 mt-10">
+      <aside>
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by
+          SafeKeyz
+        </p>
+      </aside>
+    </footer>
   );
 };
 

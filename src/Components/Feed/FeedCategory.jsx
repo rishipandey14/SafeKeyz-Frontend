e3dccb87ced54356e4405e6a2e2f04957627505d
Rishi <hopesalive1403@gmail.com>
2025-05-29 15:36:13 +0530

    fixed UI

--- src/Components/Feed/FeedCategory.jsx
+++ src/Components/Feed/FeedCategory.jsx
@@ -4,7 +4,7 @@ import FeedCard from "./FeedCard";
 const FeedCategory = ({ displayName, items, onEdit, onDelete }) => {
   return (
     <details className="border border-gray-800 rounded-xl p-4 bg-gradient-to-r from-gray-900 to-black shadow-md hover:shadow-lg transition-all duration-300">
-      <summary className="cursor-pointer text-xl font-semibold text-green-400 hover:text-white transition-colors duration-200">
+      <summary className="cursor-pointer text-xl font-semibold text-gray-300 hover:text-white transition-colors duration-200">
         {displayName}
       </summary>
       <div className="mt-4 space-y-3">

635787a4b59ae17f96fe46de5b59518da66a917c
Rishi <hopesalive1403@gmail.com>
2025-06-03 23:48:36 +0530

    fixed issue

--- src/Components/Feed/AddFeedForm.jsx
+++ src/Components/Feed/AddFeedForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useDispatch } from "react-redux";
-import { categoryFields, categoryGroups } from "./FeedHelper";
+import { categoryFields, categoryGroups } from "./feedHelper";
 import { BASE_URL } from "../../utils/constants";
 import { showToast } from "../../utils/toastSlice";
 

366d4a367fa0acddcdbb52bf03778ef9a5b51c92
Rishi <hopesalive1403@gmail.com>
2025-12-19 14:34:07 +0530

    fixed homepage issue

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import NavBar from "./NavBar";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const Body = () => {
   const user = useSelector((store) => store?.user?.user);
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
       <NavBar />
       <div className="flex flex-1">
-        {user && <Sidebar />}
+        {user && !isHomePage && <Sidebar />}
         <main className="flex-grow overflow-auto">
           <Outlet />
         </main>
